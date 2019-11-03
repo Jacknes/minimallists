59fe334ceb7b94a4a961436f080548435ec808f3
Jack Pascoe <japascoe@nine.com.au>
2019-11-03 17:41:20 +1100

    Attempts to center input field
    Please help

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,41 +1,72 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import ItemCreator from './itemCreator';
-import ListItem from './ListItem';
+import React, { useState } from "react";
+import styled from "styled-components";
+import ItemCreator from "./itemCreator";
+import ListItem from "./ListItem";
 
-type Props = {
-
-};
+type Props = {};
 
 const List: React.FC<Props> = () => {
-    const [items, setItems] = useState<string[]>([]);
+  const [items, setItems] = useState<string[]>([]);
+  const [editing, setEditing] = useState(true);
 
-    const handleCreateItem = (value: string) => {
-        setItems([...items, value]);
-    };
+  const handleCreateItem = (value: string) => {
+    setItems([...items, value]);
+  };
 
-    return (
-        <Root>
-            {items.map(item => {
-                return (
-                    <ListItem type="plain" value={item} />
-                )
-            })}
-            <ItemCreator onCreateItem={handleCreateItem} />
-        </Root>
-    )
+  const handleEditing = (editing: boolean) => {
+    setEditing(editing);
+  };
+
+  return (
+    <Root editing={editing}>
+      <ListItems>
+        {items.map(item => {
+          return <ListItem type="plain" value={item} />;
+        })}
+      </ListItems>
+      <ItemCreator onEditing={handleEditing} onCreateItem={handleCreateItem} />
+    </Root>
+  );
 };
 
-const Root = styled.div`
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    /* align-items: center; */
-    justify-content: center;
-    height: 80%;
-    margin-left: 20%;
-    margin-right: 20%;
+type RootProps = {
+  editing: boolean;
+};
+
+const Root = styled.div<RootProps>`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+   /* transform: translateY(50vh); */
+    /* transition: all 300ms; */
+  /* align-items: center; */
+  /* justify-content: flex-end; */
+  /* margin-bottom: 50%; */
+  /* height: 100%; */
+  /* justify-content: ${p => (p.editing ? "flex-end" : "flex-start")}; */
+  /* justify-content: ${p => (p.editing ? "center" : "flex-start")}; */
+  padding-top: ${p => (p.editing ? "50%" : "0%")};
+  /* height: 80%; */
+  /* margin-left: 20%;
+  margin-right: 20%; */
+  margin: 0 20%;
+  /* margin: ${p => (p.editing ? "0" : "0 20%")}; */
+  /* position: ${p => (p.editing ? "fixed" : "relative")};*/
+  /* top: unset; */
+  /* bottom: unset; */
+  /* top: 0; */
+  /* ${p => (p.editing ? "top: 50%; bottom: 50%;" : "top: 0; bottom: 0;")} */
+
+
+  /* position: fixed;
+  bottom: ${p => (p.editing ? "50%" : "0")};
+  top: ${p => (p.editing ? "auto" : "0")}; */
+  /* transform: translate(0 50%); */
+  transition: all 0.3s ease-in-out;
+  /* padding-bottom: 50%; */
+
 `;
 
+const ListItems = styled.div``;
 
 export default List;
--- src/components/itemCreator.tsx
+++ src/components/itemCreator.tsx
@@ -1,50 +1,75 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import isHotkey from "is-hotkey";
 
 type Props = {
-    onCreateItem(value: string): void;
+  onCreateItem(value: string): void;
+  onEditing(editing: boolean): void;
 };
 
-const ItemCreator: React.FC<Props> = ({ onCreateItem }) => {
-    const [value, setValue] = useState('');
+const ItemCreator: React.FC<Props> = ({ onCreateItem, onEditing }) => {
+  const [value, setValue] = useState("");
 
-    const handleOnChange = (e: any) => {
-        console.log('e', e.target.value);
-        setValue(e.target.value);
-    };
+  const handleCreateItem = () => {
+    if (value !== "") {
+      onCreateItem(value);
+      setValue("");
+    }
+  };
 
-    const handleOnBlur = () => {
-        if (value !== '') {
-            onCreateItem(value);
-            setValue('');
-        }
-    };
+  const handleOnChange = (e: any) => {
+    console.log("e", e.target.value);
+    setValue(e.target.value);
+  };
 
-    return (
-        <Root>
-            <StyledInput value={value} placeholder="New list item..." onChange={handleOnChange} onBlur={handleOnBlur} />
-        </Root>
-    )
+  const handleOnBlur = () => {
+    handleCreateItem();
+    onEditing(false);
+  };
+
+  const handleOnFocus = () => {
+    onEditing(true);
+  };
+
+  const handleOnKeyDown = (e: React.KeyboardEvent) => {
+    if (isHotkey("enter", e.nativeEvent)) {
+      console.log("enter!!");
+      handleCreateItem();
+    }
+  };
+
+  return (
+    <Root>
+      <StyledInput
+        value={value}
+        placeholder="New list item..."
+        onChange={handleOnChange}
+        onBlur={handleOnBlur}
+        onFocus={handleOnFocus}
+        onKeyDown={handleOnKeyDown}
+      />
+    </Root>
+  );
 };
 
 const Root = styled.div`
-    /* border-bottom: 1px solid #e5e5e5; */
-    display: flex;
-    /* flex: 1; */
-    padding: 8px 0;
+  /* border-bottom: 1px solid #e5e5e5; */
+  display: flex;
+  /* flex: 1; */
+  padding: 8px 0;
 `;
 
 const StyledInput = styled.input`
-    /* text-align: center; */
-    font-size: 32px;
-    outline: none;
-    border: 0;
-    width: 100%;
-    /* ::placeholder{
+  /* text-align: center; */
+  font-size: 32px;
+  outline: none;
+  border: 0;
+  width: 100%;
+  /* ::placeholder{
         text-align: center;
     }
     :focus {
-        text-align: left; 
+        text-align: left;
     } */
 `;
 
